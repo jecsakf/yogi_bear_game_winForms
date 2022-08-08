e93142bd37f4e35515c8b6266c167217339af5a9
jecsakf <jecsak.fruzsina@gmail.com>
2022-08-08 12:06:06 +0200

    Upload project.

---
+++ YogiBearGame/YogiBearGame/View/GameForm.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace YogiBearGame
+{
+    partial class GameForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this._toolLabelPicnic = new System.Windows.Forms.ToolStripStatusLabel();
+            this._toolLabelPicnicBasketCounter = new System.Windows.Forms.ToolStripStatusLabel();
+            this._toolLabelTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this._toolLabelGameTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this._menuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuFileNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this._menuFileLoadGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this._menuFileExitGame = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuSettings = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuSettingsSmall = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuSettingsMedium = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuSettingsLarge = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuTime = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuTimeStart = new System.Windows.Forms.ToolStripMenuItem();
+            this._menuTimeStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._toolLabelPicnic,
+            this._toolLabelPicnicBasketCounter,
+            this._toolLabelTime,
+            this._toolLabelGameTime});
+            this.statusStrip.Location = new System.Drawing.Point(0, 537);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(584, 24);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // _toolLabelPicnic
+            // 
+            this._toolLabelPicnic.Name = "_toolLabelPicnic";
+            this._toolLabelPicnic.Size = new System.Drawing.Size(125, 19);
+            this._toolLabelPicnic.Text = "Picked picnic baskets: ";
+            // 
+            // _toolLabelPicnicBasketCounter
+            // 
+            this._toolLabelPicnicBasketCounter.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this._toolLabelPicnicBasketCounter.Name = "_toolLabelPicnicBasketCounter";
+            this._toolLabelPicnicBasketCounter.Size = new System.Drawing.Size(17, 19);
+            this._toolLabelPicnicBasketCounter.Text = "0";
+            // 
+            // _toolLabelTime
+            // 
+            this._toolLabelTime.Name = "_toolLabelTime";
+            this._toolLabelTime.Size = new System.Drawing.Size(39, 19);
+            this._toolLabelTime.Text = "Time: ";
+            // 
+            // _toolLabelGameTime
+            // 
+            this._toolLabelGameTime.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this._toolLabelGameTime.Name = "_toolLabelGameTime";
+            this._toolLabelGameTime.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this._toolLabelGameTime.Size = new System.Drawing.Size(47, 19);
+            this._toolLabelGameTime.Text = "0:00:00";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._menuFile,
+            this._menuSettings,
+            this._menuTime});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // _menuFile
+            // 
+            this._menuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._menuFileNewGame,
+            this.toolStripSeparator1,
+            this._menuFileLoadGame,
+            this.toolStripSeparator2,
+            this._menuFileExitGame});
+            this._menuFile.Name = "_menuFile";
+            this._menuFile.Size = new System.Drawing.Size(37, 20);
+            this._menuFile.Text = "File";
+            // 
+            // _menuFileNewGame
+            // 
+            this._menuFileNewGame.Name = "_menuFileNewGame";
+            this._menuFileNewGame.Size = new System.Drawing.Size(173, 22);
+            this._menuFileNewGame.Text = "New game";
+            this._menuFileNewGame.Click += new System.EventHandler(this.MenuFileNewGame_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(170, 6);
+            // 
+            // _menuFileLoadGame
+            // 
+            this._menuFileLoadGame.Name = "_menuFileLoadGame";
+            this._menuFileLoadGame.Size = new System.Drawing.Size(173, 22);
+            this._menuFileLoadGame.Text = "Load gametables...";
+            this._menuFileLoadGame.Click += new System.EventHandler(this.MenuLoadGameTables);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(170, 6);
+            // 
+            // _menuFileExitGame
+            // 
+            this._menuFileExitGame.Name = "_menuFileExitGame";
+            this._menuFileExitGame.Size = new System.Drawing.Size(173, 22);
+            this._menuFileExitGame.Text = "Exit";
+            this._menuFileExitGame.Click += new System.EventHandler(this.MenuFileExit_Click);
+            // 
+            // _menuSettings
+            // 
+            this._menuSettings.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._menuSettingsSmall,
+            this._menuSettingsMedium,
+            this._menuSettingsLarge});
+            this._menuSettings.Name = "_menuSettings";
+            this._menuSettings.Size = new System.Drawing.Size(61, 20);
+            this._menuSettings.Text = "Settings";
+            // 
+            // _menuSettingsSmall
+            // 
+            this._menuSettingsSmall.CheckOnClick = true;
+            this._menuSettingsSmall.Name = "_menuSettingsSmall";
+            this._menuSettingsSmall.Size = new System.Drawing.Size(148, 22);
+            this._menuSettingsSmall.Text = "Small table";
+            this._menuSettingsSmall.Click += new System.EventHandler(this.MenuGameSmall_Click);
+            // 
+            // _menuSettingsMedium
+            // 
+            this._menuSettingsMedium.CheckOnClick = true;
+            this._menuSettingsMedium.Name = "_menuSettingsMedium";
+            this._menuSettingsMedium.Size = new System.Drawing.Size(148, 22);
+            this._menuSettingsMedium.Text = "Medium table";
+            this._menuSettingsMedium.Click += new System.EventHandler(this.MenuGameMedium_Click);
+            // 
+            // _menuSettingsLarge
+            // 
+            this._menuSettingsLarge.CheckOnClick = true;
+            this._menuSettingsLarge.Name = "_menuSettingsLarge";
+            this._menuSettingsLarge.Size = new System.Drawing.Size(148, 22);
+            this._menuSettingsLarge.Text = "Large table";
+            this._menuSettingsLarge.Click += new System.EventHandler(this.MenuGameLarge_Click);
+            // 
+            // _menuTime
+            // 
+            this._menuTime.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._menuTimeStart,
+            this._menuTimeStop});
+            this._menuTime.Name = "_menuTime";
+            this._menuTime.Size = new System.Drawing.Size(45, 20);
+            this._menuTime.Text = "Time";
+            // 
+            // _menuTimeStart
+            // 
+            this._menuTimeStart.Name = "_menuTimeStart";
+            this._menuTimeStart.Size = new System.Drawing.Size(98, 22);
+            this._menuTimeStart.Text = "Start";
+            this._menuTimeStart.Click += new System.EventHandler(this.MenuStart_Click);
+            // 
+            // _menuTimeStop
+            // 
+            this._menuTimeStop.Name = "_menuTimeStop";
+            this._menuTimeStop.Size = new System.Drawing.Size(98, 22);
+            this._menuTimeStop.Text = "Stop";
+            this._menuTimeStop.Click += new System.EventHandler(this.MenuStop_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Game Tables Browser";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "GameForm";
+            this.Text = "Yogi Bear Game";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem _menuFile;
+        private System.Windows.Forms.ToolStripMenuItem _menuFileNewGame;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem _menuFileLoadGame;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem _menuFileExitGame;
+        private System.Windows.Forms.ToolStripMenuItem _menuSettings;
+        private System.Windows.Forms.ToolStripMenuItem _menuSettingsSmall;
+        private System.Windows.Forms.ToolStripMenuItem _menuSettingsMedium;
+        private System.Windows.Forms.ToolStripMenuItem _menuSettingsLarge;
+        private System.Windows.Forms.ToolStripMenuItem _menuTime;
+        private System.Windows.Forms.ToolStripMenuItem _menuTimeStart;
+        private System.Windows.Forms.ToolStripMenuItem _menuTimeStop;
+        private System.Windows.Forms.ToolStripStatusLabel _toolLabelPicnicBasketCounter;
+        private System.Windows.Forms.ToolStripStatusLabel _toolLabelGameTime;
+        private System.Windows.Forms.ToolStripStatusLabel _toolLabelPicnic;
+        private System.Windows.Forms.ToolStripStatusLabel _toolLabelTime;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ YogiBearGame/YogiBearGame/View/GameForm.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YogiBearGame.Model;
+using YogiBearGame.Persistence;
+
+namespace YogiBearGame
+{
+    public partial class GameForm : Form
+    {
+        #region Fields
+
+        private IYogiBearDataAccess _dataAccess;
+        private YogiBearGameModel _model;
+        private Button[,] _buttonGrid;
+        private Timer _timer;
+        private bool _tableGenerated;
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Játékablak példányosítása.
+        /// </summary>
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Form event handlers
+
+        /// <summary>
+        /// Játékablak betöltésének eseménykezelője.
+        /// </summary>
+        private void GameForm_Load(Object sender, EventArgs e)
+        {
+            // adatelérés példányosítása
+            _dataAccess = new YogiBearFileDataAccess();
+
+            // modell létrehozása és az eseménykezelők társítása
+            _model = new YogiBearGameModel(_dataAccess);
+            _model.GameAdvanced += new EventHandler<YogiBearEventArgs>(Game_GameAdvanced);
+            _model.GameOver += new EventHandler<YogiBearEventArgs>(Game_GameOver);
+
+            // időzítő létrehozása
+            _timer = new Timer();
+            _timer.Interval = 1000;
+            _timer.Tick += new EventHandler(Timer_Tick);
+
+            KeyPreview = true; // billentyűesemények kelezése
+            KeyDown += new KeyEventHandler(ButtonGrid_KeyDown);
+
+            _menuFileNewGame.Enabled = false;
+            _menuSettingsLarge.Enabled = false;
+            _menuSettingsMedium.Enabled = false;
+            _menuSettingsSmall.Enabled = false;
+            _menuTimeStart.Enabled = false;
+            _menuTimeStop.Enabled = false;
+            _tableGenerated = false;
+        }
+
+        #endregion
+
+        #region Game event handlers
+
+        /// <summary>
+        /// Játék előrehaladásának eseménykezelője.
+        /// </summary>
+        private void Game_GameAdvanced(Object sender, YogiBearEventArgs e)
+        {
+            _toolLabelGameTime.Text = TimeSpan.FromSeconds(e.GameTime).ToString("g");
+            SetupTable();
+            // játékidő frissítése
+        }
+
+        /// <summary>
+        /// Játék végének eseménykezelője.
+        /// </summary>
+        private void Game_GameOver(Object sender, YogiBearEventArgs e)
+        {
+            _timer.Stop();
+            SetupTable();
+
+            if (e.IsWon) // győzelemtől függő üzenet megjelenítése
+            {
+                MessageBox.Show("Congratulation, you win!" + Environment.NewLine +
+                                "Game time: " + TimeSpan.FromSeconds(e.GameTime).ToString("g"),
+                                "Yogi Bear Game",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("A ranger saw you." +
+                                "Sorry, you lose!",
+                                "Yogi Bear Game",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Asterisk);
+            }
+        }
+
+        #endregion
+
+        #region Grid event handlers
+
+        /// <summary>
+        /// Gombrács eseménykezelője.
+        /// </summary>
+
+        private void ButtonGrid_KeyDown(Object sender, KeyEventArgs e)
+        {
+            if (_model.GameIsOn)
+            {
+                switch (e.KeyCode) // megkapjuk a billentyűt
+                {
+                    case Keys.A:
+                        _model.Step(0, -1);
+                        e.SuppressKeyPress = true; // az eseményt nem adjuk tovább a vezérlőnek
+                        break;
+                    case Keys.S:
+                        _model.Step(1, 0);
+                        e.SuppressKeyPress = true;
+                        break;
+                    case Keys.D:
+                        _model.Step(0, 1);
+                        e.SuppressKeyPress = true;
+                        break;
+                    case Keys.W:
+                        _model.Step(-1, 0);
+                        e.SuppressKeyPress = true;
+                        break;
+                }
+
+                //mezők frissítése
+                SetupTable();
+            }
+        }
+
+        #endregion
+
+        #region Menu event handlers
+
+        /// <summary>
+        /// Új játék eseménykezelője.
+        /// </summary>
+        private void MenuFileNewGame_Click(Object sender, EventArgs e)
+        {
+            if (_tableGenerated) RemoveTable();
+            // játéktábla és menük inicializálása
+
+            _model.NewGame();
+            switch (_model.GameTable)
+            {
+                case GameTable.Small: GenerateTable(80); this.Size = new Size(500, 580); break;
+                case GameTable.Medium: GenerateTable(70); this.Size = new Size(866, 945); break;
+                case GameTable.Large: GenerateTable(50); this.Size = new Size(928, 1002); break;
+            }
+            _tableGenerated = true;
+
+            SetupTable();
+            SetupMenus();
+
+            _timer.Start();
+            _menuTimeStop.Enabled = true;
+        }
+
+        /// <summary>
+        /// Kilépés eseménykezelője.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuFileExit_Click(Object sender, EventArgs e)
+        {
+            Boolean restartTimer = _timer.Enabled;
+            _timer.Stop();
+
+            // megkérdezzük, hogy biztos ki szeretne-e lépni
+            if (MessageBox.Show("Are you sure to leave the game?", "Yogi Bear Game", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                // ha igennel válaszol
+                Close();
+            }
+            else
+            {
+                if (restartTimer)
+                    _timer.Start();
+            }
+        }
+
+        private async void MenuLoadGameTables(Object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // ha kiválasztottunk egy fájlt
+            {
+                foreach (String file in openFileDialog1.FileNames)
+                {
+                    try
+                    {
+                        await _model.LoadGameAsync(file);
+                    }
+                    catch (YogiBearDataException)
+                    {
+                        MessageBox.Show("Game loading failed!" + Environment.NewLine + "The access path or the file format is wrong.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                _menuFileNewGame.Enabled = true;
+                _menuSettingsLarge.Enabled = true;
+                _menuSettingsMedium.Enabled = true;
+                _menuSettingsSmall.Enabled = true;
+                _menuSettingsSmall.Checked = true;
+
+                _model.GameTable = GameTable.Small;
+            }
+        }
+
+        private void MenuGameSmall_Click(Object sender, EventArgs e)
+        {
+            _model.GameTable = GameTable.Small;
+            _menuSettingsSmall.Checked = true;
+            _menuSettingsMedium.Checked = false;
+            _menuSettingsLarge.Checked = false;
+        }
+
+        private void MenuGameMedium_Click(Object sender, EventArgs e)
+        {
+            _model.GameTable = GameTable.Medium;
+            _menuSettingsSmall.Checked = false;
+            _menuSettingsMedium.Checked = true;
+            _menuSettingsLarge.Checked = false;
+        }
+
+        private void MenuGameLarge_Click(Object sender, EventArgs e)
+        {
+            _model.GameTable = GameTable.Large;
+            _menuSettingsSmall.Checked = false;
+            _menuSettingsMedium.Checked = false;
+            _menuSettingsLarge.Checked = true;
+        }
+
+        private void MenuStart_Click(Object sender, EventArgs e)
+        {
+            _menuTimeStop.Enabled = true;
+            _menuTimeStart.Enabled = false;
+            _model.GameIsOn = true;
+
+            _timer.Start();
+        }
+
+        private void MenuStop_Click(Object sender, EventArgs e)
+        {
+            _menuTimeStop.Enabled = false;
+            _menuTimeStart.Enabled = true;
+            _model.GameIsOn = false;
+
+            _timer.Stop();
+        }
+        #endregion
+
+        #region Timer event handlers
+
+        /// <summary>
+        /// Időzítő eseménykeztelője.
+        /// </summary>
+        private void Timer_Tick(Object sender, EventArgs e)
+        {
+            _model.AdvanceTime(); // játék léptetése
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Új tábla létrehozása.
+        /// </summary>
+        private void GenerateTable(int size)
+        {
+            _buttonGrid = new Button[_model.Table.Size, _model.Table.Size];
+            for (Int32 i = 0; i < _model.Table.Size; i++)
+                for (Int32 j = 0; j < _model.Table.Size; j++)
+                {
+                    _buttonGrid[i, j] = new Button();
+                    _buttonGrid[i, j].Location = new Point(5 + size * j, 35 + size * i); // elhelyezkedés
+                    _buttonGrid[i, j].Size = new Size(size, size); // méret
+                    _buttonGrid[i, j].Enabled = false; // kikapcsolt állapot
+                    _buttonGrid[i, j].TabIndex = 100 + i * _model.Table.Size + j; // a gomb számát a TabIndex-ben tároljuk
+                    _buttonGrid[i, j].FlatStyle = FlatStyle.Flat; // lapított stípus
+                    _buttonGrid[i, j].BackColor = Color.FromArgb(169,255,83);
+                    _buttonGrid[i, j].KeyDown += new KeyEventHandler(ButtonGrid_KeyDown);
+                    _buttonGrid[i, j].BackgroundImageLayout = ImageLayout.Stretch;
+                    // közös eseménykezelő hozzárendelése minden gombhoz
+
+                    Controls.Add(_buttonGrid[i, j]);
+                    // felvesszük az ablakra a gombot
+                }
+        }
+
+        /// <summary>
+        /// Tábla beállítása.
+        /// </summary>
+        private void SetupTable()
+        {
+            for (Int32 i = 0; i < _buttonGrid.GetLength(0); i++)
+            {
+                for (Int32 j = 0; j < _buttonGrid.GetLength(1); j++)
+                {
+                    switch (_model.Table.GetValue(i,j))
+                    {
+                        
+                        case 0: _buttonGrid[i, j].BackColor = Color.FromArgb(169, 255, 83); break;
+                        case 1: _buttonGrid[i, j].BackColor = Color.Black; break;
+                        case 2: _buttonGrid[i, j].BackColor = Color.Blue; break;
+                        case 3: _buttonGrid[i, j].BackColor = Color.Yellow; break;
+                        case 4: _buttonGrid[i, j].BackColor = Color.Red; break;
+                        
+                        /*
+                        case 0: _buttonGrid[i, j].Image = null; break;
+                        case 1: _buttonGrid[i, j].Image = Image.FromFile(@"C:\Users\jecsa\OneDrive - Eotvos Lorand Tudomanyegyetem\Asztal\Fruzsi Fájlok\Egyetem\5félév\EVA\Beadando_1\YogiBearGame\YogiBearGame\View\yogi_bear.png"); break;
+                        case 2: _buttonGrid[i, j].Image = Image.FromFile(@"C:\Users\jecsa\OneDrive - Eotvos Lorand Tudomanyegyetem\Asztal\Fruzsi Fájlok\Egyetem\5félév\EVA\Beadando_1\YogiBearGame\YogiBearGame\View\park_ranger.jpg"); break;
+                        case 3: _buttonGrid[i, j].Image = Image.FromFile(@"C:\Users\jecsa\OneDrive - Eotvos Lorand Tudomanyegyetem\Asztal\Fruzsi Fájlok\Egyetem\5félév\EVA\Beadando_1\YogiBearGame\YogiBearGame\View\tree.png"); break;
+                        case 4: _buttonGrid[i, j].Image = Image.FromFile(@"C:\Users\jecsa\OneDrive - Eotvos Lorand Tudomanyegyetem\Asztal\Fruzsi Fájlok\Egyetem\5félév\EVA\Beadando_1\YogiBearGame\YogiBearGame\View\picnic_basket.jpg"); break;
+                        */
+                    }
+                }
+            }
+            _toolLabelPicnicBasketCounter.Text = _model.Table.PickedBasketsCount.ToString();
+            _toolLabelGameTime.Text = TimeSpan.FromSeconds(_model.GameTime).ToString("g");
+        }
+
+        private void RemoveTable()
+        {
+            for (int i = 0; i < _model.Table.Size; i++)
+                for (int j = 0; j < _model.Table.Size; j++)
+                    Controls.Remove(_buttonGrid[i, j]);
+            _buttonGrid = null;
+        }
+
+        /// <summary>
+        /// Menük beállítása.
+        /// </summary>
+        private void SetupMenus()
+        {
+            _menuSettingsSmall.Checked = (_model.GameTable == GameTable.Small);
+            _menuSettingsMedium.Checked = (_model.GameTable == GameTable.Medium);
+            _menuSettingsLarge.Checked = (_model.GameTable == GameTable.Large);
+        }
+
+        #endregion
+    }
+}
